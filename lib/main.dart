53e5674b6bb9c4556465736c89c874750df66d60
Shivam <shivamtirath@gmail.com>
2023-08-21 10:03:45 +0530

    launcher image + sizedbox

--- lib/main.dart
+++ lib/main.dart
@@ -113,204 +113,209 @@ class _MyHomePageState extends State<MyHomePage> {
           currentFocus.unfocus();
         }
       },
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Logo(),
-              Container(
-                margin: const EdgeInsets.only(left: 20, right: 20),
-                padding: const EdgeInsets.fromLTRB(20, 50, 20, 30),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.blue.shade300,
-                        Colors.blue.shade700,
-                        Colors.pink.shade400
-                      ]),
-                  borderRadius: const BorderRadius.only(
-                      topRight: Radius.circular(40.0),
-                      bottomRight: Radius.circular(40.0),
-                      topLeft: Radius.circular(40.0),
-                      bottomLeft: Radius.circular(40.0)),
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 20.0,
                 ),
-                child: Column(
-                  children: [
-                    AnimatedTextKit(
-                      animatedTexts: [
-                        TypewriterAnimatedText(
-                          'Log in to continue',
-                          textStyle: const TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 25.0,
-                            letterSpacing: 1.0,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.black,
+                const Logo(),
+                Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20),
+                  padding: const EdgeInsets.fromLTRB(20, 50, 20, 30),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.blue.shade300,
+                          Colors.blue.shade700,
+                          Colors.pink.shade400
+                        ]),
+                    borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(40.0),
+                        bottomRight: Radius.circular(40.0),
+                        topLeft: Radius.circular(40.0),
+                        bottomLeft: Radius.circular(40.0)),
+                  ),
+                  child: Column(
+                    children: [
+                      AnimatedTextKit(
+                        animatedTexts: [
+                          TypewriterAnimatedText(
+                            'Log in to continue',
+                            textStyle: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 25.0,
+                              letterSpacing: 1.0,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black,
+                            ),
+                            speed: const Duration(milliseconds: 300),
                           ),
-                          speed: const Duration(milliseconds: 300),
-                        ),
-                      ],
-                      repeatForever: false,
-                      totalRepeatCount: 2,
-                      pause: const Duration(milliseconds: 3000),
-                      displayFullTextOnTap: true,
-                      stopPauseOnTap: true,
-                    ),
-                    const SizedBox(
-                      height: 55.0,
-                    ),
-                    TextField(
-                      textInputAction: TextInputAction.next,
-                      cursorColor: Colors.black,
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(
-                          Icons.mail_outline,
-                          size: 25.0,
-                          color: Colors.black,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.white,
-                            width: 30.0,
-                          ),
-                          borderRadius: BorderRadius.circular(35.0),
-                        ),
-                        labelText: ' Email',
-                        fillColor: Colors.blue.shade200,
-                        filled: true,
-                        labelStyle: const TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 20.0,
-                          letterSpacing: 0.2,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
-                        hintText: 'username@gmail.com',
-                        hintStyle: const TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 15.0,
-                          letterSpacing: 1.0,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black54,
-                        ),
+                        ],
+                        repeatForever: false,
+                        totalRepeatCount: 2,
+                        pause: const Duration(milliseconds: 3000),
+                        displayFullTextOnTap: true,
+                        stopPauseOnTap: true,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 30.0,
-                    ),
-                    TextField(
-                      cursorColor: Colors.black,
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        prefixIcon: const Icon(
-                          Icons.lock_outlined,
-                          size: 25.0,
-                          color: Colors.black,
-                        ),
-                        suffixIcon: GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              obs = !obs;
-                            });
-                          },
-                          child: Icon(
-                            obs
-                                ? Icons.visibility_off_outlined
-                                : Icons.visibility_outlined,
+                      const SizedBox(
+                        height: 55.0,
+                      ),
+                      TextField(
+                        textInputAction: TextInputAction.next,
+                        cursorColor: Colors.black,
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.mail_outline,
                             size: 25.0,
                             color: Colors.black,
                           ),
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                              color: Colors.white, width: 30.0),
-                          borderRadius: BorderRadius.circular(35.0),
-                        ),
-                        labelText: ' Password',
-                        fillColor: Colors.blue.shade200,
-                        filled: true,
-                        labelStyle: const TextStyle(
-                          fontFamily: 'Poppins',
-                          fontSize: 20.0,
-                          letterSpacing: 0.2,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
-                      ),
-                      obscureText: obs,
-                      onEditingComplete: _signIn,
-                    ),
-                    const SizedBox(
-                      height: 15.0,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(top: 10, left: 20),
-                      child: InkWell(
-                        onTap: () {
-                          setState(() {
-                            txt = "Contact admin/owner";
-                            Future.delayed(const Duration(milliseconds: 2000),
-                                () {
-                              setState(() {
-                                txt = "Forgot Password?";
-                              });
-                            });
-                          });
-                        },
-                        child: Text(
-                          txt,
-                          style: const TextStyle(
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.white,
+                              width: 30.0,
+                            ),
+                            borderRadius: BorderRadius.circular(35.0),
+                          ),
+                          labelText: ' Email',
+                          fillColor: Colors.blue.shade200,
+                          filled: true,
+                          labelStyle: const TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            letterSpacing: 0.2,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          ),
+                          hintText: 'username@gmail.com',
+                          hintStyle: const TextStyle(
                             fontFamily: 'Poppins',
                             fontSize: 15.0,
-                            letterSpacing: 0.0,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.black,
+                            letterSpacing: 1.0,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black54,
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 13,
-                    ),
-                    MessagePrint(
-                        success: _success, message: message, error: error),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    ClickableButton(
-                        onTap: () {
-                          _success = 1;
-                          setState(() {
-                            message = " . . . . ";
-                          });
-                          _signIn();
-                        },
-                        text: "LOG IN",
-                        c: Colors.black),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    ClickableButton(
-                        onTap: () async {
-                          Navigator.of(context).pushNamed('/signup');
-                        },
-                        text: "SIGN UP",
-                        c: Colors.black),
-                    const SizedBox(
-                      height: 10.0,
-                    ),
-                  ],
+                      const SizedBox(
+                        height: 30.0,
+                      ),
+                      TextField(
+                        cursorColor: Colors.black,
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(
+                            Icons.lock_outlined,
+                            size: 25.0,
+                            color: Colors.black,
+                          ),
+                          suffixIcon: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                obs = !obs;
+                              });
+                            },
+                            child: Icon(
+                              obs
+                                  ? Icons.visibility_off_outlined
+                                  : Icons.visibility_outlined,
+                              size: 25.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 30.0),
+                            borderRadius: BorderRadius.circular(35.0),
+                          ),
+                          labelText: ' Password',
+                          fillColor: Colors.blue.shade200,
+                          filled: true,
+                          labelStyle: const TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 20.0,
+                            letterSpacing: 0.2,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                          ),
+                        ),
+                        obscureText: obs,
+                        onEditingComplete: _signIn,
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.only(top: 10, left: 20),
+                        child: InkWell(
+                          onTap: () {
+                            setState(() {
+                              txt = "Contact admin/owner";
+                              Future.delayed(const Duration(milliseconds: 2000),
+                                  () {
+                                setState(() {
+                                  txt = "Forgot Password?";
+                                });
+                              });
+                            });
+                          },
+                          child: Text(
+                            txt,
+                            style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 15.0,
+                              letterSpacing: 0.0,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 13,
+                      ),
+                      MessagePrint(
+                          success: _success, message: message, error: error),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      ClickableButton(
+                          onTap: () {
+                            _success = 1;
+                            setState(() {
+                              message = " . . . . ";
+                            });
+                            _signIn();
+                          },
+                          text: "LOG IN",
+                          c: Colors.black),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      ClickableButton(
+                          onTap: () async {
+                            Navigator.of(context).pushNamed('/signup');
+                          },
+                          text: "SIGN UP",
+                          c: Colors.black),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-            ],
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
           ),
         ),
       ),
